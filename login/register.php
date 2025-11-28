6c72f3f76d4e3a981997b38fe16546d22a483ce5
Maame Dankwaa Afranie-Adjei <144915734+maame-dankwaa@users.noreply.github.com>
2025-11-28 12:39:00 +0000

    commit

--- login/register.php
+++ login/register.php
@@ -5,6 +5,10 @@ if (session_status() === PHP_SESSION_NONE) {
 
 require_once __DIR__ . '/../classes/User.php';
 
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
+
 $error = '';
 $old = [];
 
